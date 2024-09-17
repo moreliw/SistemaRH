38718b9d35a9c3712e0f0177bdce6d2e00f59363
William Moreli <william.moreli@globalsys.com.br>
2024-09-16 22:30:24 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  cor = false;
   title = 'SistemaRH';
+
+  trocarCor() {
+    this.cor = !this.cor;
+    console.log('this.cor');
+    if (this.cor) {
+      this.title = 'Alex & Patrão <3';
+    } else {
+      this.title = 'William';
+    }
+  }
 }
